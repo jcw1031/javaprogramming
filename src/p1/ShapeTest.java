1d9958522e0c1b9ca344eb20190d0c5d9a613c1a
woopaca <jcw001031@gmail.com>
2022-05-20 21:27:14 +0900

    complete 'MartTest'

--- src/p1/ShapeTest.java
+++ src/p1/ShapeTest.java
@@ -1,9 +1,10 @@
 package p1;
-import java.net.spi.URLStreamHandlerProvider;
+
 import java.util.*;
 
 class Shape{
-    int a, b, c;
+    private int a, b, c;
+
     public Shape(int a){
         this.a=a;
         System.out.println("직선의 길이는 : "+a);
@@ -25,19 +26,32 @@ class Shape{
 
 public class ShapeTest {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
-        int[] a= new int[] {Integer.parseInt(null)};
-        int i;
+        String input;
         Shape ss;
 
-        System.out.println("수치를 입력하세요 (1개 : 직선, 2개 : 직사각형, 3개 : 직육면체)");
-        for(i=0;i<3;i++){
-            a[i]=s.nextInt();
-            if(a[i]==Integer.parseInt(null)) break;
+        while (true) {
+            System.out.println("수치를 입력하세요 (1개 : 직선, 2개 : 직사각형, 3개 : 직육면체)");
+            input = s.nextLine();
+            String[] split = (input.split(" "));
+            if (split.length == 0 || split.length > 3) {
+                System.out.println("1개 ~ 3개의 수치를 입력하세요.");
+            } else {
+                int[] figure = new int[split.length];
+                for (int i = 0; i < figure.length; i++) {
+                    figure[i] = Integer.parseInt(split[i]);
+                }
+                if (figure.length == 1) {
+                    ss = new Shape(figure[0]);
+                    break;
+                } else if (figure.length == 2) {
+                    ss = new Shape(figure[0], figure[1]);
+                    break;
+                } else
+                    ss = new Shape(figure[0], figure[1], figure[2]); 
+                break;
+            }
         }
-        if(i==0) ss=new Shape(a[0]);
-        else if(i==1) ss=new Shape(a[0], a[1]);
-        else ss= new Shape(a[0], a[1], a[2]);
     }
 }
